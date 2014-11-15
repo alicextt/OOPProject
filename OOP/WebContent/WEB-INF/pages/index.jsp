9cecf7959d5b6bcf0002e300aa09427ae52ac143
Tingting Xu <txu@scu.edu>
2014-11-15 14:38:54 -0800

    test session for user and login logic

--- OOP/WebContent/WEB-INF/pages/index.jsp
+++ OOP/WebContent/WEB-INF/pages/index.jsp
@@ -1,12 +1,16 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"  
-    pageEncoding="ISO-8859-1"%>  
+    pageEncoding="ISO-8859-1"%> 
+<%@ page import="model.User" %>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">  
 <html>  
 <head>  
 <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">  
 <title>Struts2 tutorial</title>  
 </head>  
-<body bgColor="lightblue">  
-Hello world!!!This is Struts 2 tutorial  
+<body >  
+Hello ${sessionScope.user.getLastName()} ,${sessionScope.user.getFirstName()}!
+You are logged in! 
+
+
 </body>  
 </html> 
--- OOP/src/com/login/LoginAction.java
+++ OOP/src/com/login/LoginAction.java
@@ -1,76 +1,91 @@
 package com.login;
 
-
-
 import java.io.PrintWriter;
 import java.util.List;
+import java.util.Map;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+import org.apache.struts2.interceptor.SessionAware;
+
 import model.User;
 
-import com.opensymphony.xwork2.ActionSupport;  
-  
-public class LoginAction extends ActionSupport {  
-  
-    private String userName;  
-    private String password;  
-  
-    public String execute() {  
-    	
-        return SUCCESS;  
-    }  
-  
-     
-    public String getUserName() {  
-        return userName;  
-    }  
-  
-    public void setUserName(String userName) {  
-        this.userName = userName;  
-    }  
-  
-    public String getPassword() {  
-        return password;  
-    }  
-  
-    public void setPassword(String password) {  
-        this.password = password;  
-    }  
-  
-    public void validate() {  
-//        if (getUserName().length() == 0) {  
-//            addFieldError("userName", "UserName.required");  
-//        } else if (!getUserName().equals("Arpit")) {  
-//            addFieldError("userName", "Invalid User");  
-//        }  
-//        if (getPassword().length() == 0) {  
-//            addFieldError("password", getText("password.required"));  
-//        }  
-        System.out.println("We are in the validate method");
+import com.opensymphony.xwork2.ActionSupport;
+
+public class LoginAction extends ActionSupport implements SessionAware {
+
+	private String userName;
+	private String password;
+	private User u;
+	Map<String, Object> session;
+
+	@Override
+	public void setSession(Map<String, Object> session) {
+		this.session = session;
+	}
+
+	public String execute() {
+	
+		session.put("user", u);
+		return SUCCESS;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public void validate() {
+		// if (getUserName().length() == 0) {
+		// addFieldError("userName", "UserName.required");
+		// } else if (!getUserName().equals("Arpit")) {
+		// addFieldError("userName", "Invalid User");
+		// }
+		// if (getPassword().length() == 0) {
+		// addFieldError("password", getText("password.required"));
+		// }
+//		System.out.println("We are in the validate method");
 		String username = "user";
 		String password = "root";
 		String un = getUserName();
 		String pw = getPassword();
-		EntityManagerFactory emf=Persistence.createEntityManagerFactory("User");
-		EntityManager em=emf.createEntityManager();
-		javax.persistence.Query querybyusername=em.createNamedQuery("User.findByusername");
-		
+		EntityManagerFactory emf = Persistence
+				.createEntityManagerFactory("User");
+		EntityManager em = emf.createEntityManager();
+		javax.persistence.Query querybyusername = em
+				.createNamedQuery("User.findByusername");
+
 		querybyusername.setParameter("userName", un);
-//		System.out.print(querybyusername);
-		List<User> result=querybyusername.getResultList();
-//		for(User p:result){
-//			System.out.println(p.getPassWord());
-//		}
-		
+		// System.out.print(querybyusername);
+		List<User> result = querybyusername.getResultList();
+		// for(User p:result){
+		// System.out.println(p.getPassWord());
+		// }
+
 		em.close();
 		emf.close();
+		if (result.isEmpty()) {
+			addFieldError("userName", "Incorrect Account !");
+			return;
+		}
 		if (!pw.equals(result.get(0).getPassWord())) {
-          addFieldError("password", "wrong password");  
-		} 
+			addFieldError("password", "Incorrect Password!");
+			return;
+		}
+		u=result.get(0);
+	}
 
-    }  
-}  
-
+}
