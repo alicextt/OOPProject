440761d4d9c5f8515e813fbcab45a482105bd551
Lu Yu <Lydia@Lus-MacBook-Air.local>
2014-11-18 11:15:12 -0800

    budgettable change

--- Database/Frugal_budget_table.sql
+++ Database/Frugal_budget_table.sql
@@ -1,10 +1,10 @@
-create table if not exists budget(
-budget_id int(10),
-description text,
-project_id int(10),
-projected_amount int(10),
-actual_amount int(10),
-primary key (budget_id));
+create table if not exists Budget(
+IdBudget int(10),
+Description text,
+IdProject int(10),
+ProjectedAmount int(10),
+ActualAmount int(10),
+primary key (IdBudget));
 
-insert into budget values('401', 'buget for 401', '201', '10000', '12000');
-insert into budget values('402', 'buget for 402', '202', '10000', '9000');
+insert into Budget values('401', 'buget for 401', '201', '10000', '12000');
+insert into Budget values('402', 'buget for 402', '202', '10000', '9000');
