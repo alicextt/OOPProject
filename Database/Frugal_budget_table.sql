a741a1fe2691e564517ce750705d6a935759ab1c
Lu Yu <Lydia@Lus-MacBook-Air.local>
2014-11-18 09:34:01 -0800

    budget table sql

---
+++ Database/Frugal_budget_table.sql
@@ -0,0 +1,10 @@
+create table if not exists budget(
+budget_id int(10),
+description, text,
+project_id int(10),
+projected_amount, int(10),
+actual_amount, int(10),
+primary key budget_id);
+
+insert into budget values('401', 'buget for 401', '201', '10000', '12000');
+insert into budget values('402', 'buget for 402', '202', '10000', '9000');
