876d3605fb00b475562be873b2a88092ea67a76a
Lu Yu <Lydia@Lus-MacBook-Air.local>
2014-11-18 11:21:10 -0800

    project table change

--- Database/project_table.sql
+++ Database/project_table.sql
@@ -7,8 +7,8 @@ create table if not exists Project(
 	LastModifiedDate Date, 
 	ProjectOutcome text, 
 	ProjectStatus varchar(20),  
-	primary key (IdPoject));
+	primary key (IdProject));
 
-insert into Project values('201', 'Energy Made in Uganda', 'Energy Made in Uganda description', '2014-01-01', '2015-01-01', '2014-06-01', 'outcome unknown','In progress', '10000');
+insert into Project values('201', 'Energy Made in Uganda', 'Energy Made in Uganda description', '2014-01-01', '2015-01-01', '2014-06-01', 'outcome unknown','In progress');
 
-insert into Project values('202', 'Dynamic Poverty Heat Map', 'Dynamic Poverty Heat Map description', '2014-01-01', '2015-01-01', '2014-06-01', 'outcome unknown','In progress', '10000');
+insert into Project values('202', 'Dynamic Poverty Heat Map', 'Dynamic Poverty Heat Map description', '2014-01-01', '2015-01-01', '2014-06-01', 'outcome unknown','In progress');
